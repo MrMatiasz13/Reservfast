2eb8f836f5446244e79871ee3849ced841579e1f
MrMatiasz13 <grzeskowmateusz1328@gmail.com>
2024-07-25 14:28:10 +0200

    Project setup

---
+++ app/src/main/java/pl/mrmatiasz/reservfastapp/ReservfastApp.kt
@@ -0,0 +1,7 @@
+package pl.mrmatiasz.reservfastapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReservfastApp: Application()
---
+++ app/src/main/java/pl/mrmatiasz/reservfastapp/module/AppModule.kt
@@ -0,0 +1,9 @@
+package pl.mrmatiasz.reservfastapp.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule
