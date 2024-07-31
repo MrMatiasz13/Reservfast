926be28058a8eb244b8f7737570293adbccaa37c
MrMatiasz13 <grzeskowmateusz1328@gmail.com>
2024-07-31 11:05:27 +0200

    Added simple UI to display a hotel list

--- app/src/main/java/pl/mrmatiasz/reservfastapp/presentation/main_screen/MainScreen.kt
+++ app/src/main/java/pl/mrmatiasz/reservfastapp/presentation/main_screen/MainScreen.kt
@@ -1,8 +1,7 @@
 package pl.mrmatiasz.reservfastapp.presentation.main_screen
 
 import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
-import android.util.Log
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
@@ -18,7 +19,6 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.PlaylistAddCheckCircle
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -32,13 +32,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -81,6 +82,8 @@ fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
     var selectedItemIndex by remember { mutableIntStateOf(0) }
 
     val scope = rememberCoroutineScope()
+
+    val itemList = viewModel.hotelsList.collectAsState()
 
     ModalNavigationDrawer(
         drawerContent = {
@@ -131,7 +134,9 @@ fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
                     .padding(padding)
             ) {
                 LazyColumn(modifier = Modifier.fillMaxWidth()) {
-
+                    items(itemList.value) { item ->
+                        HotelListItem(testHotel = item)
+                    }
                 }
             }
         }
@@ -140,7 +145,11 @@ fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
 
 @Composable
 fun HotelListItem(testHotel: Hotel) {
-    Box(modifier = Modifier.fillMaxWidth()) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(2.dp, Color.Black, RoundedCornerShape(8.dp))
+    ) {
         Row(modifier = Modifier.padding(8.dp)) {
             Column {
                 Text(
